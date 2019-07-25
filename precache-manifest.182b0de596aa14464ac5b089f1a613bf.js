ba08cda7a94cbe9d944c364aad56023ce1bff396
rodergstep <rodergstep@gmail.com>
2019-07-25 21:01:45 +0300

    initial

---
+++ precache-manifest.182b0de596aa14464ac5b089f1a613bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7ade2c8693e86fca830e",
+    "url": "/static/js/main.7ade2c86.chunk.js"
+  },
+  {
+    "revision": "3c51b958f763800bf818",
+    "url": "/static/js/1.3c51b958.chunk.js"
+  },
+  {
+    "revision": "7ade2c8693e86fca830e",
+    "url": "/static/css/main.1544db73.chunk.css"
+  },
+  {
+    "revision": "3c51b958f763800bf818",
+    "url": "/static/css/1.d01f21d0.chunk.css"
+  },
+  {
+    "revision": "cd9d413dc068bc4904b48d9166f49dff",
+    "url": "/index.html"
+  }
+];
